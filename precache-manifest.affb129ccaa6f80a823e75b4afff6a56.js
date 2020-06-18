58a745e7a4994cea43b48f8ba1219cae1c9b97d2
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2020-06-18 13:19:54 +0200

    Updates

---
+++ precache-manifest.affb129ccaa6f80a823e75b4afff6a56.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d11a2ad8049cbbfddddd5956815148",
+    "url": "/varvet/index.html"
+  },
+  {
+    "revision": "12b34b09a5e372cb625e",
+    "url": "/varvet/static/css/2.f4ea3d46.chunk.css"
+  },
+  {
+    "revision": "4afa6798511a1310a8d3",
+    "url": "/varvet/static/css/main.ae8518e1.chunk.css"
+  },
+  {
+    "revision": "12b34b09a5e372cb625e",
+    "url": "/varvet/static/js/2.6508c615.chunk.js"
+  },
+  {
+    "revision": "a108bd14c69faf1507e1074a6c8a5de4",
+    "url": "/varvet/static/js/2.6508c615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4afa6798511a1310a8d3",
+    "url": "/varvet/static/js/main.4ccd9b28.chunk.js"
+  },
+  {
+    "revision": "27d0b1ba3cb349315082",
+    "url": "/varvet/static/js/runtime-main.9b341f86.js"
+  },
+  {
+    "revision": "e89638307efb2088ee047b073d64d9f0",
+    "url": "/varvet/static/media/af.e8963830.png"
+  },
+  {
+    "revision": "3ae373fa31cc2820113f2669cfc99195",
+    "url": "/varvet/static/media/ar.3ae373fa.png"
+  },
+  {
+    "revision": "6827f261416397cc9982ae6ca30ab770",
+    "url": "/varvet/static/media/br.6827f261.png"
+  },
+  {
+    "revision": "2ac94b0f6afd455b71e4d700ba040e2f",
+    "url": "/varvet/static/media/cd.2ac94b0f.png"
+  },
+  {
+    "revision": "d052e0b27c5b6d0dc6e0d117e4cdeaa5",
+    "url": "/varvet/static/media/do.d052e0b2.png"
+  },
+  {
+    "revision": "e6cc1f96ed55f4c5ece03ce3e38aa88b",
+    "url": "/varvet/static/media/ec.e6cc1f96.png"
+  },
+  {
+    "revision": "e362f359a3f68b188ab67dfef60b8b12",
+    "url": "/varvet/static/media/eg.e362f359.png"
+  },
+  {
+    "revision": "c828c44606e29d01873343461fdb4550",
+    "url": "/varvet/static/media/er.c828c446.png"
+  },
+  {
+    "revision": "8c819b2f3d8c6dc7e0637c2441dc6fa2",
+    "url": "/varvet/static/media/es.8c819b2f.png"
+  },
+  {
+    "revision": "c15face77db6dfc87a680fd96ee31cc7",
+    "url": "/varvet/static/media/gt.c15face7.png"
+  },
+  {
+    "revision": "98d52d179f363fa421c21946b51f1511",
+    "url": "/varvet/static/media/hk.98d52d17.svg"
+  },
+  {
+    "revision": "7bd313de9856c7d31351eb2e371ed9cf",
+    "url": "/varvet/static/media/hr.7bd313de.png"
+  },
+  {
+    "revision": "e0c2b4f59ee723b1979928e682caf85b",
+    "url": "/varvet/static/media/kr.e0c2b4f5.png"
+  },
+  {
+    "revision": "b33dbf1534fdbc4951e0a266a3cdc36b",
+    "url": "/varvet/static/media/lk.b33dbf15.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/varvet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "45ad2f667e09002e6ecaf5f5e212f2da",
+    "url": "/varvet/static/media/mx.45ad2f66.png"
+  },
+  {
+    "revision": "26df812a9d250efa0723b8d4d35a1bd1",
+    "url": "/varvet/static/media/ni.26df812a.png"
+  },
+  {
+    "revision": "e75d5f2f5744bebde4e8caa45d9923d6",
+    "url": "/varvet/static/media/ph.e75d5f2f.png"
+  },
+  {
+    "revision": "67bf8f9e91bd1462d48bedbce86d2cfa",
+    "url": "/varvet/static/media/pt.67bf8f9e.png"
+  },
+  {
+    "revision": "d583be7ec8813f27d82ee0f77232aa6b",
+    "url": "/varvet/static/media/rs.d583be7e.png"
+  },
+  {
+    "revision": "01c13e57ce441176c6faa88ef6a1d30e",
+    "url": "/varvet/static/media/sa.01c13e57.png"
+  },
+  {
+    "revision": "8f52682c02d9c20e292d016f5d64fb1b",
+    "url": "/varvet/static/media/sv.8f52682c.png"
+  },
+  {
+    "revision": "e4a75107b2413eda9113052a8eb1daaa",
+    "url": "/varvet/static/media/us.e4a75107.png"
+  }
+]);
